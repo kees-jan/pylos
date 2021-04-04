13c7c096d9e21fc22ddcbfdebc1136d63a1f665b
Kees-Jan Dijkzeul <kees-jan@dijkzeul.eu>
2021-04-04 13:15:27 +0200

    Tidying up

--- pylos/test/test_move.cc
+++ pylos/test/test_move.cc
@@ -1,65 +1,81 @@
 #include <array>
 #include <bitset>
 #include <cassert>
+#include <gsl/gsl_assert>
+#include <gsl/gsl_util>
 
 #include <catch2/catch.hpp>
+
+#define Bug() GSL_CONTRACT_CHECK("Bug", false)
 
 namespace pylos
 {
   constexpr uint32_t                               number_of_layers = 4;
-  constexpr std::array<uint32_t, number_of_layers> number_of_balls_in_layer{16, 9, 4, 1};
+  constexpr std::array<uint32_t, number_of_layers> number_of_balls_in_layer_{16, 9, 4, 1};
 
-  constexpr auto number_of_balls_in_lower_layers = [] {
+  [[nodiscard]] constexpr uint32_t number_of_balls_in_layer(uint32_t l) { return gsl::at(number_of_balls_in_layer_, l); }
+
+  constexpr auto number_of_balls_in_lower_layers_ = [] {
     std::array<uint32_t, number_of_layers + 1> t{};
     uint32_t                                   sum{0};
 
     for(uint32_t i = 0; i < number_of_layers; i++)
     {
-      t[i] = sum;
-      sum += number_of_balls_in_layer[i];
+      gsl::at(t, i) = sum;
+      sum += number_of_balls_in_layer(i);
     }
     t[number_of_layers] = sum;
     return t;
   }();
 
-  constexpr uint32_t size_of_layer(uint32_t l)
+  [[nodiscard]] constexpr uint32_t number_of_balls_in_lower_layers(uint32_t l)
   {
-    assert(l < number_of_layers);
+    return gsl::at(number_of_balls_in_lower_layers_, l);
+  }
+
+  [[nodiscard]] constexpr uint32_t size_of_layer(uint32_t l)
+  {
+    Expects(l < number_of_layers);
     return number_of_layers - l;
   }
 
-  class integer_position
+  class [[nodiscard]] integer_position
   {
   public:
     explicit constexpr integer_position(uint32_t i)
       : position(i)
     {
-      assert(i < 30);
+      Expects(i < 30);
     }
 
-    constexpr uint32_t x() { return (position - number_of_balls_in_lower_layers[layer()]) % size_of_layer(layer()); }
+    [[nodiscard]] constexpr uint32_t x() const
+    {
+      return (position - number_of_balls_in_lower_layers(layer())) % size_of_layer(layer());
+    }
 
-    constexpr uint32_t y() { return (position - number_of_balls_in_lower_layers[layer()]) / size_of_layer(layer()); }
+    [[nodiscard]] constexpr uint32_t y() const
+    {
+      return (position - number_of_balls_in_lower_layers(layer())) / size_of_layer(layer());
+    }
 
-    constexpr uint32_t layer()
+    [[nodiscard]] constexpr uint32_t layer() const
     {
       for(uint32_t l = 0; l < number_of_layers; l++)
       {
-        if(number_of_balls_in_lower_layers[l + 1] > position)
+        if(number_of_balls_in_lower_layers(l + 1) > position)
         {
           return l;
         }
       }
 
-      assert(false);
+      Bug();
     }
-
 
   private:
     uint32_t position;
   };
 
-  class coordinate_position
+  class [[nodiscard]] coordinate_position
   {
   public:
     constexpr coordinate_position(uint32_t x_, uint32_t y_, uint32_t layer_)
@@ -67,16 +83,19 @@ namespace pylos
       , y(y_)
       , layer(layer_)
     {
-      assert(layer < number_of_layers);
-      assert(x < size_of_layer(layer));
-      assert(y < size_of_layer(layer));
+      Expects(layer < number_of_layers);
+      Expects(x < size_of_layer(layer));
+      Expects(y < size_of_layer(layer));
     }
 
     explicit constexpr coordinate_position(integer_position i)
       : coordinate_position(i.x(), i.y(), i.layer())
     {}
 
-    constexpr bool operator==(const coordinate_position& other) { return x == other.x && y == other.y && layer == other.layer; }
+    [[nodiscard]] constexpr bool operator==(const coordinate_position& other) const
+    {
+      return x == other.x && y == other.y && layer == other.layer;
+    }
 
   private:
     uint32_t x;
@@ -93,11 +112,11 @@ using namespace pylos;
 
 TEST_CASE("Board positions")
 {
-  STATIC_REQUIRE(number_of_balls_in_lower_layers[number_of_layers] == 30);
+  STATIC_REQUIRE(number_of_balls_in_lower_layers_[number_of_layers] == 30);
 
   for(uint32_t l = 0; l < number_of_layers; l++)
   {
-    REQUIRE(number_of_balls_in_layer[l] == size_of_layer(l) * size_of_layer(l));
+    REQUIRE(number_of_balls_in_layer(l) == size_of_layer(l) * size_of_layer(l));
   }
 
   REQUIRE(all_positions().count() == 30);
